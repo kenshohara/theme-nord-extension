ac3f20b2f6bdc4cfb0362a2443b52d4ae36a9f17
kenshohara <kensho.hara@aist.go.jp>
2019-06-10 23:11:13 +0900

    implement theme

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,27 @@
-import {
-  JupyterLab, JupyterLabPlugin
-} from '@jupyterlab/application';
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
 
-import '../style/index.css';
+import { JupyterLab, JupyterLabPlugin } from '@jupyterlab/application';
 
+import { IThemeManager } from '@jupyterlab/apputils';
 
 /**
- * Initialization data for the theme-nord-extension extension.
+ * A plugin for the Jupyter Nord Theme.
  */
-const extension: JupyterLabPlugin<void> = {
-  id: 'theme-nord-extension',
-  autoStart: true,
-  activate: (app: JupyterLab) => {
-    console.log('JupyterLab extension theme-nord-extension is activated!');
-  }
+const plugin: JupyterLabPlugin<void> = {
+  id: '@jupyterlab/theme-nord-extension:plugin',
+  requires: [IThemeManager],
+  activate: (app: JupyterLab, manager: IThemeManager) => {
+    const style = '@jupyterlab/theme-nord-extension/index.css';
+
+    manager.register({
+      name: 'JupyterLab Nord',
+      isLight: false,
+      load: () => manager.loadCSS(style),
+      unload: () => Promise.resolve(undefined)
+    });
+  },
+  autoStart: true
 };
 
-export default extension;
+export default plugin;
