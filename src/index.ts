01f2bb9fa6f5a26a413e8e0534856b77bb0c0e8f
kenshohara <kensho.hara@aist.go.jp>
2019-09-27 17:49:06 +0900

    update based on jupyter lab v1.0.2

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,26 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-import { JupyterLab, JupyterLabPlugin } from '@jupyterlab/application';
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
 
 import { IThemeManager } from '@jupyterlab/apputils';
 
 /**
  * A plugin for the Jupyter Nord Theme.
  */
-const plugin: JupyterLabPlugin<void> = {
+const plugin: JupyterFrontEndPlugin<void> = {
   id: '@kenshohara/theme-nord-extension:plugin',
   requires: [IThemeManager],
-  activate: (app: JupyterLab, manager: IThemeManager) => {
+  activate: (app: JupyterFrontEnd, manager: IThemeManager) => {
     const style = '@kenshohara/theme-nord-extension/index.css';
 
     manager.register({
       name: 'JupyterLab Nord',
       isLight: false,
+      themeScrollbars: true,
       load: () => manager.loadCSS(style),
       unload: () => Promise.resolve(undefined)
     });
